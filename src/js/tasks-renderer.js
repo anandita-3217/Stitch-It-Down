f712993a92c3b8697cc3eff49106b04cb965a0d4
Anandita <dakshugarimella@gmail.com>
2025-06-21 10:16:51 +0530

    added placeholder code in tasks.html

--- src/js/tasks-renderer.js
+++ src/js/tasks-renderer.js
@@ -2,8 +2,9 @@
 import '@css/main.css'
 import '@css/components/timer.css';
 import '@css/components/sidebar.css';
+import '@css/components/tasks.css';
 import '@components/sidebar.js';
-import '@components/timer.js';
+
 
 // PNG imports
 import stitchHappy from '@assets/images/characters/stitch-happy.png';
